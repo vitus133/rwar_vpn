a9c8d25f2235a0e760ff78b72df2aba0ad45c2a7
vitaly <vitus@dr.com>
2020-03-01 00:27:40 +0200

    client creation

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,95 @@ openvpn --genkey --secret /etc/openvpn/server/tc.key
 # Create the DH parameters file using the predefined ffdhe2048 group
 
 openssl dhparam -out /etc/openvpn/server/dh.pem 2048
+public_ip=$(curl -4Ls "http://whatismyip.akamai.com/")
+protocol="udp"
+port="1194"
+# Generate server.conf
+echo "local $public_ip
+port $port
+proto $protocol
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh.pem
+auth SHA512
+tls-crypt tc.key
+topology subnet
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt" > /etc/openvpn/server/server.conf
+echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server/server.conf
+# DNS
+echo 'push "dhcp-option DNS 1.1.1.1"' >> /etc/openvpn/server/server.conf
+echo 'push "dhcp-option DNS 1.0.0.1"' >> /etc/openvpn/server/server.conf
+echo "keepalive 10 120
+cipher AES-256-CBC
+user nobody
+group nobody
+persist-key
+persist-tun
+status openvpn-status.log
+verb 3
+crl-verify crl.pem" >> /etc/openvpn/server/server.conf
+echo "explicit-exit-notify" >> /etc/openvpn/server/server.conf
+# Enable net.ipv4.ip_forward for the system
+echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
+# Enable without waiting for a reboot or service restart
+echo 1 > /proc/sys/net/ipv4/ip_forward
+# Create a service to set up persistent iptables rules
+echo "[Unit]
+Before=network.target
+[Service]
+Type=oneshot
+ExecStart=/sbin/iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $public_ip
+ExecStart=/sbin/iptables -I INPUT -p $protocol --dport $port -j ACCEPT
+ExecStart=/sbin/iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
+ExecStart=/sbin/iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+ExecStop=/sbin/iptables -t nat -D POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $public_ip
+ExecStop=/sbin/iptables -D INPUT -p $protocol --dport $port -j ACCEPT
+ExecStop=/sbin/iptables -D FORWARD -s 10.8.0.0/24 -j ACCEPT
+ExecStop=/sbin/iptables -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+RemainAfterExit=yes
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/openvpn-iptables.service
+systemctl enable --now openvpn-iptables.service
 
+# client-common.txt is created so we have a template to add further users later
+echo "client
+dev tun
+proto $protocol
+remote $public_ip $port
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+auth SHA512
+cipher AES-256-CBC
+ignore-unknown-option block-outside-dns
+block-outside-dns
+verb 3" > /etc/openvpn/server/client-common.txt
+# Enable and start the OpenVPN service
+systemctl enable --now openvpn-server@server.service
+new_client () {
+	# Generates the custom client.ovpn
+	{
+	cat /etc/openvpn/server/client-common.txt
+	echo "<ca>"
+	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	echo "</ca>"
+	echo "<cert>"
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$1".crt
+	echo "</cert>"
+	echo "<key>"
+	cat /etc/openvpn/server/easy-rsa/pki/private/"$1".key
+	echo "</key>"
+	echo "<tls-crypt>"
+	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+	echo "</tls-crypt>"
+	} > ~/"$1".ovpn
+}
+new_client "client"
 
 cd /usr/share/rwar_vpn
 # Install and start web server
