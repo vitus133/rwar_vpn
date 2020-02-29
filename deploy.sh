04937da58c505f056eeaf815b9549383491c2e61
vitaly <vitus@dr.com>
2020-02-29 23:52:06 +0200

    fixed tgz

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ yum install wget python3 git openvpn iptables openssl ca-certificates -y
 cd /usr/share
 git clone https://github.com/vitus133/rwar_vpn.git
 cd rwar_vpn
-tar xzf bin/EasyRSA-nix-3.0.5 -C ~/
+tar xzf bin/EasyRSA-nix-3.0.5.tgz -C ~/
 mv ~/EasyRSA-3.0.5/ /etc/openvpn/server/
 mv /etc/openvpn/server/EasyRSA-3.0.5/ /etc/openvpn/server/easy-rsa/
 chown -R root:root /etc/openvpn/server/easy-rsa/
